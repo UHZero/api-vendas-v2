2688dd90b7257a0bb217a68be9de3c2bae564740
uhzero <aureo.moraes@gmail.com>
2022-09-19 02:19:03 -0300

    install jest and create fake repository

---
+++ src/modules/costumers/domain/repositories/fakes/FakeCustomersRepository.ts
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from 'uuid';
+import Customer from '@modules/costumers/infra/typeorm/entities/Customer';
+import { ICreateCustomer } from '@modules/costumers/domain/model/ICreateCustomer';
+import { ICustomersRepository } from '@modules/costumers/domain/repositories/ICustomersRepository';
+
+class FakeCustomersRepository
+  implements Omit<ICustomersRepository, 'remove' | 'findAll'>
+{
+  private customers: Customer[] = [];
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = new Customer();
+
+    customer.id = uuidv4();
+    customer.name = name;
+    customer.email = email;
+
+    this.customers.push(customer);
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    Object.assign(this.customers, customer);
+
+    return customer;
+  }
+
+  // public async remove(customer: Customer): Promise<void> {}
+
+  // public async findAll(): Promise<Customer[] | undefined> {}
+
+  public async findByName(name: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.name === name);
+    return customer;
+  }
+
+  public async findById(id: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.id === id);
+    return customer;
+  }
+
+  public async findByEmail(email: string): Promise<Customer | undefined> {
+    const customer = this.customers.find(customer => customer.email === email);
+    return customer;
+  }
+}
+
+export default FakeCustomersRepository;
